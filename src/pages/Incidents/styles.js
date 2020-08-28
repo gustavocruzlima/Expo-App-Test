6c0d49d4c43330f2c9c69685c02a5a1e5efaea56
gustavocruzlima <gustavofcruzlima@gmail.com>
2020-08-28 16:18:54 -0300

    inicio da aplicaçao, tela inicial e estrutura de pastas. correçao

---
+++ src/pages/Incidents/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+    },
+
+    header:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    headerText: {
+        fontSize: 15,
+        color: '#737380'
+    },
+
+    headerTextBold: {
+        fontWeight: 'bold'
+    },
+
+    title:{
+        fontSize:30,
+        marginTop: 16,
+        color: '#13131a',
+        fontWeight: 'bold'
+    }
+});
